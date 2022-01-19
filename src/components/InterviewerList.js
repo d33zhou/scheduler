b95327ef65aec6a5bdbe7d0ce1bab11185e25c5c
David Zhou <d33zhou@outlook.com>
2022-01-19 16:54:56 -0500

    refactor list&item to not pass id as prop

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,11 +5,10 @@ import InterviewerListItem from './InterviewerListItem';
 export default function InterviewerList(props) {
   const interviewerArray = props.interviewers.map(interviewer => <InterviewerListItem
     key={interviewer.id}
-    id={interviewer.id}
     name={interviewer.name}
     avatar={interviewer.avatar}
     selected={interviewer.id === props.interviewer}
-    setInterviewer={props.setInterviewer}
+    setInterviewer={() => props.setInterviewer(interviewer.id)}
     />);
 
   return (
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,14 +3,13 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const { id, name, avatar, selected, setInterviewer } = props;
-  const handleClick = () => setInterviewer(id);
+  const { name, avatar, selected, setInterviewer } = props;
   const interviewerClass = classNames('interviewers__item', {
     'interviewers__item--selected': selected
   });
   
   return (
-    <li className={interviewerClass} onClick={handleClick}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img
         className="interviewers__item-image"
         src={avatar}
