3b3f9a5ea2ac6e4db91811000d1a2c03080597e5
David Zhou <d33zhou@outlook.com>
2022-01-19 19:15:22 -0500

    refactor day/setDay to value/onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,8 +6,8 @@ function DayList(props) {
     key={day.id}
     name={day.name}
     spots={day.spots}
-    selected={day.name === props.day}
-    setDay={props.setDay}
+    selected={day.name === props.value}
+    setDay={props.onChange}
     />);
   
   return (
