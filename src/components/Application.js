f7b1d41785a7093873604cbce8727cddd9901833
David Zhou <d33zhou@outlook.com>
2022-01-20 21:12:22 -0500

    update logic for Promise.all axios requests and integrate API data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,67 +3,36 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
-
-// Mock test data for Appointments
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   
   const [ state, setState] = useState({
     day: "Monday",
     days: [],
-    //appointments: {},
+    appointments: {}
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const appointmentsArray = appointments.map(appointment => <Appointment
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+    ]).then(all => {      
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+    });
+
+  }, []);
+
+  const appointmentsArray = dailyAppointments.map(appointment => <Appointment
     key={appointment.id}
     {...appointment}
     />);
   appointmentsArray.push(<Appointment key="last" time="5pm" />);
-
-  useEffect(() => {
-    axios.get("/api/days").then(res => setDays(res.data));
-  }, []);
 
   return (
     <main className="layout">
