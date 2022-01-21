922c158c5c05a1397e7f627dbedd8711847e3ec9
David Zhou <d33zhou@outlook.com>
2022-01-21 18:07:58 -0500

    use axios to call interviewers API, set up helper function to process return API and update logic to handle data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,35 +3,45 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   
   const [ state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-
   const setDay = day => setState({ ...state, day });
-
+  
   useEffect(() => {
-
+    
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
+      axios.get('/api/interviewers'),
     ]).then(all => {      
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
-
+    
   }, []);
+  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const appointmentsArray = dailyAppointments.map(appointment => <Appointment
-    key={appointment.id}
-    {...appointment}
-    />);
+  const appointmentsArray = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
   appointmentsArray.push(<Appointment key="last" time="5pm" />);
 
   return (
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,14 @@ export function getAppointmentsForDay(state, day) {
 
   return appointments;
 }
+
+export function getInterview(state, interview) {
+  if (interview) {
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer]
+    };
+  }
+
+  return null;
+}
