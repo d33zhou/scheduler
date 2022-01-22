502f51ed0eb5e458ee3805d67a2da6fd89d63462
David Zhou <d33zhou@outlook.com>
2022-01-21 19:36:27 -0500

    add custom hook logic and test cases

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,33 @@
 import { useState } from 'react';
 
 export default function useVisualMode(initial) {
-  const [ mode, setMode ] = useState(initial);
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+
+    if (replace) {
+      const newHistory = history.slice(0, history.length - 1);
+      setHistory([...newHistory, newMode]);
+    } else {
+      setHistory([...history, newMode]);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+  
+      setMode(history[newHistory.length - 1]);
+      setHistory(newHistory);
+    }
+  }
   
   return {
     mode,
+    transition,
+    back,
   };
 }
