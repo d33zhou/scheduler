7f061782f119fda5369836b514be86187411d558
David Zhou <d33zhou@outlook.com>
2022-01-26 01:51:36 -0500

    add tests for form and implement name validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   
   const { interviewers, onSave, onCancel } = props;
   
@@ -16,6 +17,15 @@ export default function Form(props) {
   function cancel() {
     reset();
     onCancel();
+  }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    onSave(student, interviewer);
   }
 
   return (
@@ -32,6 +42,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={interviewers}
           value={interviewer}
@@ -41,7 +52,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
         </section>
       </section>
     </main>
